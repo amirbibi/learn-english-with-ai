06cfb323b3f562846c4bf6bb44133581fbf27b5e
Amir Bibi <amirbi595@gmail.com>
2024-08-01 11:52:13 +0300

    Moved concept type to separate file

--- client/src/types/api.ts
+++ client/src/types/api.ts
@@ -10,10 +10,6 @@ export interface ValidateTokenResponse {
   email: string;
 }
 
-export interface Concept {
-  name: string;
-}
-
 export interface EvaluationResponse {
   evaluation: string;
   goodDescription: string;
